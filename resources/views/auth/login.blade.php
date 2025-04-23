83120b9c0f539bd5367c6a5b2951d1f9361437bb
HondaAkihito <honaki.and.1114@gmail.com>
2025-04-23 20:52:38 +0900

    /views/auth/login.blade.php バリデーション位置 修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,21 +7,21 @@
 
         <!-- Email Address -->
         <div>
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
             <x-input-label for="email" :value="__('auth.email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                
             <x-input-label for="password" :value="__('auth.password')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
                             name="password"
                             required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Remember Me -->
