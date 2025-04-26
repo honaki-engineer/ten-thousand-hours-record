b41dd154b89a349712a7b1be04516bfd3b98fdb2
HondaAkihito <honaki.and.1114@gmail.com>
2025-04-26 20:55:34 +0900

    /views/emails/reset-password.blade.php インデント調整 メール本文のデザインのため

--- resources/views/emails/reset-password.blade.php
+++ resources/views/emails/reset-password.blade.php
@@ -1,14 +1,14 @@
 @component('mail::message')
-    # パスワード再設定のご案内
+# パスワード再設定のご案内
 
-    {{ $user->name ?? 'お客' }}様、こんにちは！
+{{ $user->name ?? 'お客' }}様、こんにちは！
 
-    パスワード再設定のリクエストを受け付けました。
+パスワード再設定のリクエストを受け付けました。
 
-    @component('mail::button', ['url' => $resetUrl])
-        パスワードを再設定する
-    @endcomponent
+@component('mail::button', ['url' => $resetUrl])
+    パスワードを再設定する
+@endcomponent
 
-    このリンクは60分で無効になります。
-    心当たりがない場合は、このメールを無視してください。
+このリンクは60分で無効になります。
+心当たりがない場合は、このメールを無視してください。
 @endcomponent
