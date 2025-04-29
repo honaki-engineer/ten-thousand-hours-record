1e18b682027100121912e7f8403e2e3aa04cabb0
HondaAkihito <honaki.and.1114@gmail.com>
2025-04-29 12:29:19 +0900

    /views/emails/reset-password.blade.php 再設定メール本文で改行処理を追記

--- lang/ja/auth.php
+++ lang/ja/auth.php
@@ -32,9 +32,9 @@ return [
     // パスワード再設定メール
     'Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.' => '「ご登録のメールアドレス」を入力していただければ、<br>パスワード再設定用のリンクをメールでお送りします。',
     'email_password_reset_link' => '再設定用メールを受け取る',
+    
+    // PW再設定画面
     'new_password' => '新しいパスワード',
     'confirm_new_password' => '新しいパスワード(再確認)',
-
-    // PW再設定画面
     'reset_password' => 'パスワードを再設定する',
 ];
