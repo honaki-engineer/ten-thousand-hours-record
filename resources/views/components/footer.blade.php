a568343a3c832abbd83af5fe3041c3ae4cc022fc
HondaAkihito <honaki.and.1114@gmail.com>
2025-05-11 21:22:49 +0900

    footer デザイン修正

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- resources/views/components/footer.blade.php -->
-<footer class="bg-white shadow bg-opacity-60">
-    <div class="max-w-7xl mx-auto py-1 px-2 sm:px-6 lg:px-8 text-center text-gray-500 text-xs xl:text-sm">
+<footer>
+    <div class="shadow bg-white py-4 text-center text-xs xl:text-sm text-gray-500">
         &copy; {{ date('Y') }} {{ config('app.name') }}. All rights reserved.
     </div>
 </footer>
