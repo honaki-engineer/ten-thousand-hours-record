4862908f0bede1282c320b3c4d1e9b54ab259cb5
HondaAkihito <honaki.and.1114@gmail.com>
2025-05-12 18:41:02 +0900

    /views/components/footer.blade.phptext-xs 削除

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- resources/views/components/footer.blade.php -->
 <footer>
-    <div class="shadow bg-white py-4 text-center text-xs xl:text-sm text-gray-500">
+    <div class="shadow bg-white py-4 text-center text-sm text-gray-500">
         &copy; {{ date('Y') }} {{ config('app.name') }} All rights reserved
     </div>
 </footer>
