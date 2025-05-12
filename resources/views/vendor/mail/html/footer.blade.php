a734b73bc62a1282ce5139ddc1ce13d46074d697
HondaAkihito <honaki.and.1114@gmail.com>
2025-05-12 20:58:43 +0900

    PW再設定メール fooer文 修正

--- resources/views/vendor/mail/html/footer.blade.php
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -1,11 +1,15 @@
 <tr>
-<td>
-<table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
-<tr>
-<td class="content-cell" align="center">
-© {{ date('Y') }} {{ config('app.name') }}. All rights reserved.
-</td>
-</tr>
-</table>
-</td>
-</tr>
+  <td>
+  <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+  <tr>
+  <td class="content-cell" align="center">
+  {{-- {{ Illuminate\Mail\Markdown::parse($slot) }} --}}
+  &copy;
+  <span style="margin-left: 4px;">{{ date('Y') }}</span>
+  <span style="margin-left: 4px;">{{ config('app.name') }}</span>
+  <span style="margin-left: 4px;">All rights reserved</span>
+  </td>
+  </tr>
+  </table>
+  </td>
+  </tr>
