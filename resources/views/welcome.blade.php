b9783bbfaf3de5e725306798ec90a9fac5e83ad0
HondaAkihito <honaki.and.1114@gmail.com>
2025-05-12 18:28:15 +0900

    /views/welcome.blade.php tailwindcssCDN 削除

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,6 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>ファーストビュー</title>
-        <script src="https://cdn.tailwindcss.com"></script>
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
