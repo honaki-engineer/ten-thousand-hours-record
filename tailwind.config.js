6992fda2b2264bf9dcec3aaf94efaeb8c86c9783
HondaAkihito <honaki.and.1114@gmail.com>
2025-05-13 18:04:44 +0900

    /tailwind.config.js safelist追記

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,12 @@ module.exports = {
         'mb-2',
         // プロフィール画面(削除)
         'sm:w-3/4',
+
+        // authのフォーム
+        'sm:max-w-md',
+        'md:max-w-lg',
+        'lg:max-w-xl',
+        'xl:max-w-2xl'
       ],
 
     theme: {
