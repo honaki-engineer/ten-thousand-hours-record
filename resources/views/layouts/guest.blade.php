cfed849712f784f099dd260b7730830541b6f8cf
HondaAkihito <honaki.and.1114@gmail.com>
2025-05-13 11:05:30 +0900

    /views/layouts/guest.blade.php フォーム幅が狭くなっていた問題  修正

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,19 +17,19 @@
 </head>
 
 <body class="font-sans text-gray-900 antialiased min-h-screen flex flex-col bg-gray-100">
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div class="flex-grow flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
+    <div class="flex-grow flex flex-col justify-center">
+        <div class="flex justify-center">
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
         </div>
-        <x-footer-auth />
+    
+        <div class="w-full sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl mx-auto mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            {{ $slot }}
+        </div>
     </div>
+    
+    <x-footer-auth />
 </body>
 
 </html>
