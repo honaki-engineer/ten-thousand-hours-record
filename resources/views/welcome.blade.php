8d83898300c4c28f6431e89ad2be6b31839cf171
HondaAkihito <honaki.and.1114@gmail.com>
2025-05-16 16:36:12 +0900

    <title>修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>ファーストビュー</title>
+        <title>{{ config('app.name', '1万時間学習記録') }}</title>
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
